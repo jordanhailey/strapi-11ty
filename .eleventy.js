1cd82aa13a5982e29ff8ec9b9aa5295e7b39c13b
Jordan Hailey <jordan.m.hailey@gmail.com>
2021-07-27 14:04:06 -0700

    adding axios, eleventy error plugin, and html-minifier packages

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,25 @@
+const HtmlMin = require('html-minifier');
+const ErrorOverlay = require('eleventy-plugin-error-overlay');
+
 module.exports = (eleventyConfig) => {
-	
+	eleventyConfig.setTemplateFormats(['md']);
+  eleventyConfig.addPlugin(ErrorOverlay);
+  eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
+    if (outputPath.endsWith('.html')) {
+      let minified = HtmlMin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+    return content;
+  });
 	return {
 		dir: {
 			input: "11ty",
 			includes: '_templates',
-		}
+		},
+		jsDataFileSuffix: '.data',
 	}
 }
